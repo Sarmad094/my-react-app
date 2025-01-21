9c94afaa675d5c13d07cad2b0553528b280216be
Sarmad094 <sarmad04@outlook.com>
2025-01-21 13:52:43 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Karusu hello! - running on Azure - deployed by oneklzubby using a complex 
+          Karusu hello! - running on Azure - deployed by oneklzubby using a complex --2
         </p>
         <a
           className="App-link"
