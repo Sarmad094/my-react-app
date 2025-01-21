7bd7e4f1c4c7b76a5d46c7cd17ce81bc08727772
Sarmad <sarmad04@outlook.com>
2025-01-21 13:27:11 +0100

    changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Karusu hello! - running on Azure - deployed by oneklzubby using a complex 
         </p>
         <a
           className="App-link"
